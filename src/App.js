6df4f0763c4705e75b743dce74d80306645f2a83
ngocng <ngoc.nguyen1@gbst.com>
2019-01-02 15:33:11 +1000

    Using json api Todos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import $ from 'jquery';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
+import Todos from './Components/Todos';
 import './App.css';
 
 
@@ -9,12 +11,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      projects: []
+      projects: [],
+      todos: []
     }
   }
 
-  // life cycle method to set up data when component is renderred
-  componentWillMount() {
+  getProjects() {
     this.setState({projects: [
       {
         id: uuid.v4(),
@@ -32,6 +34,32 @@ class App extends Component {
         category: 'Web Development'
       }
     ]});
+  }
+
+  getTodos(){
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/todos',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({todos: data}, function(){
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+      }
+    })
+  }
+
+  // life cycle method to set up data when component is renderred
+  componentWillMount() {
+    this.getProjects();
+    this.getTodos();
+  }
+
+  componentDidMount() {
+    this.getTodos();
   }
 
   handleAddProject(project){
@@ -53,6 +81,8 @@ class App extends Component {
       <div className="App">
         <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+        <hr />
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
